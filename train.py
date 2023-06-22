d12de6c6c22620e8d0efb0cf79ef9cc786366b74
cassanof <federico.cassano@federico.codes>
2023-06-22 12:44:50 -0700

    patch cld

--- train.py
+++ train.py
@@ -140,7 +140,6 @@ class ConstantLengthDataset(IterableDataset):
             seq_length (int): Length of token sequences to return.
             num_of_sequences (int): Number of token sequences to keep in buffer.
             chars_per_token (int): Number of characters per token used to estimate number of tokens in text buffer.
-            seed (int): Seed for random number generator.
     """
 
     def __init__(
@@ -152,19 +151,15 @@ class ConstantLengthDataset(IterableDataset):
         num_of_sequences=1024,
         chars_per_token=3.6,
         content_field="content",
-        seed=0,
     ):
         self.tokenizer = tokenizer
-        self.concat_token_id = (
-            tokenizer.eos_token_id if tokenizer.eos_token_id else args.eos_token_id
-        )
+        self.concat_token_id = tokenizer.eos_token_id if tokenizer.eos_token_id is not None else args.eos_token_id
         self.dataset = dataset
         self.seq_length = seq_length
         self.infinite = infinite
         self.current_size = 0
         self.max_buffer_size = seq_length * chars_per_token * num_of_sequences
         self.content_field = content_field
-        self.seed = seed
 
     def __iter__(self):
         iterator = iter(self.dataset)
@@ -186,25 +181,16 @@ class ConstantLengthDataset(IterableDataset):
             tokenized_inputs = self.tokenizer(
                 buffer, truncation=False)["input_ids"]
             all_token_ids = []
-
             for tokenized_input in tokenized_inputs:
-                if not tokenized_input is None:
-                    all_token_ids.extend(
-                        tokenized_input + [self.concat_token_id])
-
-            examples = []
+                all_token_ids.extend(tokenized_input + [self.concat_token_id])
             for i in range(0, len(all_token_ids), self.seq_length):
                 input_ids = all_token_ids[i: i + self.seq_length]
                 if len(input_ids) == self.seq_length:
-                    examples.append(input_ids)
-
-            random.shuffle(examples)
-            for example in examples:
-                self.current_size += 1
-                yield {
-                    "input_ids": torch.LongTensor(example),
-                    "labels": torch.LongTensor(example),
-                }
+                    self.current_size += 1
+                    yield {
+                        "input_ids": torch.LongTensor(input_ids),
+                        "labels": torch.LongTensor(input_ids),
+                    }
 
 
 def create_datasets(tokenizer, args):
@@ -242,7 +228,6 @@ def create_datasets(tokenizer, args):
         seq_length=args.seq_length,
         chars_per_token=chars_per_token,
         content_field=args.data_column,
-        seed=args.seed,
     )
     valid_dataset = ConstantLengthDataset(
         tokenizer,
@@ -251,7 +236,6 @@ def create_datasets(tokenizer, args):
         seq_length=args.seq_length,
         chars_per_token=chars_per_token,
         content_field=args.data_column,
-        seed=args.seed,
     )
 
     return train_dataset, valid_dataset
