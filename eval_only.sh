4c2fdf67c7ff592155923be140616f24b2fb2452
cassanof <federico.cassano@federico.codes>
2023-07-25 10:40:09 -0700

    delete eval scripts, start to rethink eval pipeline

--- eval_only.sh
+++
@@ -1,5 +0,0 @@
-accelerate launch  bigcode-evaluation-harness/main.py \
-  --model Salesforce/codegen2-7B \
-  --tasks multiple-lua \
-  --allow_code_execution \
-  --load_generations_path generations.json
