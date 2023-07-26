b2ca94eac6c7c672cfd103ec8b93324b1b38eb39
cassanof <federico.cassano@federico.codes>
2023-07-26 12:04:15 -0700

    more modules!

--- train.py
+++ train.py
@@ -297,7 +297,8 @@ def run_training(args, train_data, val_data):
             lora_dropout=args.lora_dropout,
             bias="none",
             task_type="CAUSAL_LM",
-            target_modules=["c_proj", "c_attn", "q_attn"]
+            target_modules=["c_proj", "c_attn", "q_attn", "q_proj",
+                            "k_proj", "v_proj", "out_proj", "fc_in", "fc_out", "wte"]
         )
 
         model = get_peft_model(model, lora_config)
