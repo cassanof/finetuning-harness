c53a909540d708fb8933a6f43374c3bae079dd71
cassanof <federico.cassano@federico.codes>
2023-07-26 21:09:10 -0700

    main

--- number_of_tokens.py
+++ number_of_tokens.py
@@ -20,25 +20,26 @@ def get_total_tokens(dataset, tokenizer, data_column, nb_examples):
     return total_tokens
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--tokenizer", type=str,
-                    default="./starcoder_tokenizer_files")
-parser.add_argument("--dataset", type=str,
-                    default="nuprl/stack_dedup_lua_codegen")
-args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tokenizer", type=str,
+                        default="./starcoder_tokenizer_files")
+    parser.add_argument("--dataset", type=str,
+                        default="nuprl/stack_dedup_lua_codegen")
+    args = parser.parse_args()
 
-print("Loading tokenizer")
-tokenizer = AutoTokenizer.from_pretrained(
-    args.tokenizer
-)
+    print("Loading tokenizer")
+    tokenizer = AutoTokenizer.from_pretrained(
+        args.tokenizer
+    )
 
-print("Loading dataset")
-dataset = load_dataset(
-    args.dataset,
-    split="train")
+    print("Loading dataset")
+    dataset = load_dataset(
+        args.dataset,
+        split="train")
 
-print("Tokenizing dataset")
-data_column = "content"
-total_tokens = get_total_tokens(
-    dataset, tokenizer, data_column, nb_examples=len(dataset))
-print(f"Total number of tokens in dataset: {total_tokens}")
+    print("Tokenizing dataset")
+    data_column = "content"
+    total_tokens = get_total_tokens(
+        dataset, tokenizer, data_column, nb_examples=len(dataset))
+    print(f"Total number of tokens in dataset: {total_tokens}")
