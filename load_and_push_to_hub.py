d3613ae7e2771d535acfe20bbb16e021f690fffa
cassanof <federico.cassano@federico.codes>
2023-07-26 22:26:18 -0700

    prints

--- load_and_push_to_hub.py
+++ load_and_push_to_hub.py
@@ -8,15 +8,22 @@ parser.add_argument("--model", type=str, required=True)
 parser.add_argument("--peft", type=str)
 args = parser.parse_args()
 
+print(f"{args.model} ---> {args.push}")
+
+print("Loading tokenizer")
 tokenizer = AutoTokenizer.from_pretrained(
     args.tokenizer if args.tokenizer else args.model,
-    trust_remote_code=True,
 )
+print("Loading model")
 model = AutoModelForCausalLM.from_pretrained(args.model)
 if args.peft:
     from peft import PeftModel
+    print("Loading PEFT")
     model = PeftModel.from_pretrained(model, args.peft)
+    print("Merging and unloading")
     model = model.merge_and_unload()
+
+print("Pushing to hub")
 
 model.push_to_hub(args.push, private=True)
 tokenizer.push_to_hub(args.push, private=True)
