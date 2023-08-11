d2d6ee9431e255643c9f71b6858d702b87ed8734
cassanof <federico.cassano@federico.codes>
2023-08-11 09:26:20 -0700

    ocaml script

--- run_starcoder.sh
+++ run_starcoder.sh
@@ -5,21 +5,21 @@ if [ $# -eq 0 ]
     exit 1
 fi
 python3 -m torch.distributed.launch \
-        --nproc_per_node 4 \
+        --nproc_per_node 8 \
         train.py \
         --deepspeed="$1" \
         --model_path="bigcode/starcoderbase" \
         --no_custom_tokenizer \
-        --dataset_name="nuprl/stack_dedup_lua_codegen" \
-        --output_dir="./model_starcoder_lua50k" \
+        --dataset_name="nuprl/multiplt-ocaml-dedup-30k" \
+        --output_dir="./model_starcoder_ocaml_paper" \
         --seq_length 2048 \
         --epochs 10 \
         --batch_size 1 \
-        --gradient_accumulation_steps 8 \
+        --gradient_accumulation_steps 4 \
         --learning_rate 2e-5 \
         --num_warmup_steps 10 \
         --num_workers=$(expr $(nproc --all) - 4) \
         --no_fp16 \
         --bf16 \
-        --perc_valid_set 0.01 \
+        --perc_valid_set 0.0025 \
         --save_total_limit 20
