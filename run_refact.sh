faab5f83db5f1d9a42b20330eff7c88582bf1371
cassanof <federico.cassano@federico.codes>
2023-09-05 14:56:45 -0400

    refact config

---
+++ run_refact.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# need to give deepspeed config file as argument and language as second argument
+
+
+# throw error if deepspeed and language not given as argument
+if [ -z "$1" ]
+  then
+    echo "No deepspeed config file supplied"
+    exit 1
+fi
+if [ -z "$2" ]
+  then
+    echo "No language supplied"
+    exit 1
+fi
+T_LANG=$2
+python3 -m torch.distributed.launch \
+        --nproc_per_node 8 \
+        train.py \
+        --deepspeed="$1" \
+        --model_path="smallcloudai/Refact-1_6B-fim" \
+        --no_custom_tokenizer \
+        --dataset_name="nuprl/MultiPL-T" \
+        --split="$T_LANG" \
+        --no_approx_tokens \
+        --output_dir="./model_refact_multiplt_$T_LANG" \
+        --seq_length 2048 \
+        --epochs 5 \
+        --batch_size 1 \
+        --gradient_accumulation_steps 4 \
+        --learning_rate 2e-5 \
+        --num_warmup_steps 10 \
+        --num_workers=$(expr $(nproc --all) - 4) \
+        --no_fp16 \
+        --bf16 \
+        --perc_valid_set 0.0025 \
+        --save_total_limit 20
