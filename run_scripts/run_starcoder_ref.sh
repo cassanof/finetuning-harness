b64eb1dd12088eda566b70557eb67d9ea087c96d
cassanof <federico.cassano@federico.codes>
2023-09-28 12:12:16 -0400

    ref cfg

---
+++ run_scripts/run_starcoder_ref.sh
@@ -0,0 +1,24 @@
+# need to give deepspeed config file as argument
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied. Please give deepspeed config file as argument"
+    exit 1
+fi
+python3 -m torch.distributed.launch \
+        --nproc_per_node 4 \
+        main.py \
+        --deepspeed="$1" \
+        --model_path="bigcode/starcoderbase" \
+        --dataset_name="nuprl-staging/py_reflection_bootstrap_2500_train" \
+        --output_dir="./model_starcoder_bootstrap_ref" \
+        --seq_length 4096 \
+        --epochs 10 \
+        --batch_size 1 \
+        --gradient_accumulation_steps 8 \
+        --learning_rate 2e-5 \
+        --num_warmup_steps 10 \
+        --num_workers=$(expr $(nproc --all) - 4) \
+        --no_fp16 \
+        --bf16 \
+        --perc_valid_set 0.0025 \
+        --save_total_limit 20
