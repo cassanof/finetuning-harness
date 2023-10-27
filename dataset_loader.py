2800a1ff672af5bd967ac839424b65f749ca41f3
cassanof <federico.cassano@federico.codes>
2023-10-27 17:28:20 -0400

    changed padding logic

--- dataset_loader.py
+++ dataset_loader.py
@@ -13,7 +13,6 @@ class ConstantLengthDataset(IterableDataset):
             seq_length (int): Length of token sequences to return.
             num_of_sequences (int): Number of token sequences to keep in buffer.
             chars_per_token (int): Number of characters per token used to estimate number of tokens in text buffer.
-            reruns (int): Number of times to rerun the dataset.
     """
 
     def __init__(
@@ -25,7 +24,6 @@ class ConstantLengthDataset(IterableDataset):
         num_of_sequences=1024,
         chars_per_token=3.6,
         content_field="content",
-        reruns=1,
         concat_token_id=None,
     ):
         self.tokenizer = tokenizer
@@ -37,12 +35,10 @@ class ConstantLengthDataset(IterableDataset):
         self.current_size = 0
         self.max_buffer_size = seq_length * chars_per_token * num_of_sequences
         self.content_field = content_field
-        self.reruns = reruns
 
     def __iter__(self):
         iterator = iter(self.dataset)
         more_examples = True
-        reruns = self.reruns
         while more_examples:
             buffer, buffer_len = [], 0
             while True:
@@ -52,9 +48,8 @@ class ConstantLengthDataset(IterableDataset):
                     buffer.append(next(iterator)[self.content_field])
                     buffer_len += len(buffer[-1])
                 except StopIteration:
-                    if self.infinite or reruns > 0:
+                    if self.infinite:
                         iterator = iter(self.dataset)
-                        reruns -= 1
                     else:
                         more_examples = False
                         break
@@ -80,11 +75,13 @@ class ConstantLengthDataset(IterableDataset):
 
 class PaddedDataset(IterableDataset):
     """
-    Unlike ConstantLengthDataset this dataset returns padded sequences of tokens, which
-    have a fixed length of seq_length. The dataset will panic if a sequence is longer
+    Unlike ConstantLengthDataset this dataset returns padded sequences of tokens concatenated together,
+    which all have a fixed length of seq_length. The dataset will panic if a sequence is longer
     than seq_length, except if trim_longer is set to True, in which case the sequence
     will be trimmed to seq_length. It is important to set pad_token_id to the id of the
-    padding token in the tokenizer, otherwise the padding will not work.
+    padding token in the tokenizer, otherwise the model will be trained on a wrong padding token.
+    By default, we set the pad_token_id to the pad_token_id of the tokenizer, if it exists,
+    otherwise we set it to 0. The padding is done at the end of the concatenated sequence (right padding).
     """
 
     def __init__(
@@ -93,55 +90,101 @@ class PaddedDataset(IterableDataset):
         dataset,
         infinite=False,
         seq_length=2048,
-        trim_longer=False,
         content_field="content",
+        concat_token_id=None,
         pad_token_id=None,
+        trim_longer=False,
     ):
         self.tokenizer = tokenizer
-        if pad_token_id is not None:
-            self.tokenizer.pad_token_id = pad_token_id
-        elif self.tokenizer.pad_token_id is None:
-            # default to 0
-            self.tokenizer.pad_token_id = 0
-        else:
-            # we good, we have a pad token id preset
-            pass
-
-        print(f"Padded token id (PAD token): {self.tokenizer.pad_token_id}")
+        self.concat_token_id = concat_token_id if concat_token_id is not None else tokenizer.eos_token_id
         self.dataset = dataset
         self.seq_length = seq_length
         self.infinite = infinite
-        self.current_size = 0
         self.content_field = content_field
         self.trim_longer = trim_longer
+        if pad_token_id is not None:
+            self.pad_token_id = pad_token_id
+        elif self.tokenizer.pad_token_id is None:
+            # default to 0
+            self.pad_token_id = 0
+        else:
+            # we good, we have a pad token id preset
+            self.pad_token_id = self.tokenizer.pad_token_id
+
+        print(f"Concat token id (EOS token): {self.concat_token_id}")
+        print(f"Pad token id: {self.pad_token_id}")
 
     def __iter__(self):
         iterator = iter(self.dataset)
         more_examples = True
-
+        prev_iter_skipped = None
         while more_examples:
-            try:
-                content = next(iterator)[self.content_field]
-                tokenized_input = self.tokenizer(
-                    content,
-                    max_length=self.seq_length,
-                    truncation=self.trim_longer,
-                    padding='max_length',
-                    return_tensors="pt"
-                )["input_ids"].squeeze(0)
+            buffer, buffer_len = [], 0
+            while True:
+                try:
+                    if prev_iter_skipped is not None:
+                        encoded = prev_iter_skipped
+                        prev_iter_skipped = None
+                    else:
+                        new = next(iterator)[self.content_field]
+                        encoded = self.tokenizer.encode(
+                            new) + [self.concat_token_id]
 
-                # Check the length if truncation is not allowed
-                if not self.trim_longer and len(tokenized_input) > self.seq_length:
-                    raise ValueError(
-                        f"A sequence is longer than {self.seq_length} tokens and trim_longer is set to False")
+                    if len(encoded) > self.seq_length:
+                        if self.trim_longer:
+                            encoded = encoded[:self.seq_length -
+                                              1] + [self.concat_token_id]
 
-                self.current_size += 1
-                yield {
-                    "input_ids": tokenized_input,
-                    "labels": tokenized_input.clone(),
-                }
-            except StopIteration:
-                if self.infinite:
-                    iterator = iter(self.dataset)
-                else:
-                    more_examples = False
+                        else:
+                            raise ValueError(
+                                f"Sequence of length {len(encoded)} is longer than seq_length {self.seq_length}."
+                            )
+
+                    if len(encoded) + buffer_len > self.seq_length:
+                        prev_iter_skipped = encoded
+                        break
+
+                    buffer.append(encoded)
+                    buffer_len += len(encoded)
+                except StopIteration:
+                    if self.infinite:
+                        iterator = iter(self.dataset)
+                    else:
+                        more_examples = False
+                        break
+
+            assert buffer_len <= self.seq_length
+
+            # concatenate all sequences
+            token_ids = []
+            for tokenized_input in buffer:
+                token_ids.extend(tokenized_input)
+
+            # pad to seq_length
+            token_ids.extend([self.pad_token_id] *
+                             (self.seq_length - len(token_ids)))
+
+            yield {
+                "input_ids": torch.LongTensor(token_ids),
+                "labels": torch.LongTensor(token_ids),
+            }
+
+
+if __name__ == "__main__":
+    # testing out the padded dataset
+    import datasets
+    from transformers import AutoTokenizer
+    ds = datasets.load_dataset(
+        "nuprl-staging/multiplt-python-instrs-5k-train-codellama", split="train")
+    tokenizer = AutoTokenizer.from_pretrained("codellama/CodeLlama-13b-hf")
+    dataset = PaddedDataset(tokenizer, ds, seq_length=2048)
+    num_exs = 0
+    for i, example in enumerate(dataset):
+        #  print("#" * 80)
+        print(f"Example {i}")
+        decoded = tokenizer.decode(example["input_ids"])
+        num_exs += decoded.count("### Instruction:")
+        #  print(decoded)
+
+    print(f"Total number of examples: {num_exs}")
+    print(f"Total number of examples in the dataset: {len(ds)}")
