7225a1c7b27718f9528a2d7986592681f612e383
cassanof <federico.cassano@federico.codes>
2024-03-03 21:11:13 -0500

    loss for mamba

--- train.py
+++ train.py
@@ -122,6 +122,20 @@ class BetterTrainer(Trainer):
 
         return output
 
+    def compute_loss(self, model, inputs, return_outputs=False):
+        input_ids = inputs.pop("input_ids")
+        lm_logits = model(input_ids)[0]
+
+        labels = input_ids.to(lm_logits.device)
+        shift_logits = lm_logits[:, :-1, :].contiguous()
+        labels = labels[:, 1:].contiguous()
+
+        loss_fct = torch.nn.CrossEntropyLoss()
+        lm_loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), labels.view(-1))
+
+        return lm_loss
+
+
     def _trl_activate_neftune(self, model):  # NOTE: copypasted from TRL
         r"""
         Activates the neftune as presented in this code: https://github.com/neelsjain/NEFTune and paper: https://arxiv.org/abs/2310.05914
